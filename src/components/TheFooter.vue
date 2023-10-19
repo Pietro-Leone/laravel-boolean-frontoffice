670f41586be79faeac78dfb8a9c226bfb2e4c617
AndreaLinza <andrealinza@gmail.com>
2023-10-19 19:40:38 +0200

    add footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+
+    data() {
+        return {
+
+        };
+    },
+
+    methods: {
+
+
+    },
+};
+</script>
+
+<template>
+
+<footer class="bg-dark text-white">
+    <div class="container">
+        <div class="row row-cols-4">
+            <div class="col">
+                <h5>Contattaci:</h5>
+                <ul>
+                    <li>999999999</li>
+                    <li>348 2505200</li>
+                    <li>1515121321</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h5>API Utilizzata:</h5>
+                <a href="#"></a>
+            </div>
+            <div class="col">
+                <h5>Chi Siamo</h5>
+                
+                
+            </div>
+            <div class="col">
+                
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
+</template>
+
+<style scoped lang="scss"></style>
