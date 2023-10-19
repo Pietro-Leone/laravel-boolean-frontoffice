b1950ccca9b22932111f1104d76a78ffc2754c5e
ElyPoli <itsos.poli@gmail.com>
2023-10-19 18:33:54 +0200

    create routers

--- src/router.js
+++ src/router.js
@@ -1,7 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [
+import Home from "./pages/Home.vue";
+import Contacts from "./pages/Contacts.vue";
+import AboutUs from "./pages/AboutUs.vue";
 
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/chi-siamo",
+    name: "chi-siamo",
+    component: Contacts,
+  },
+  {
+    path: "/contatti",
+    name: "contatti",
+    component: AboutUs,
+  },
 ];
 
 const router = createRouter({
