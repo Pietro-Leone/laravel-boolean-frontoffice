2b2bcfd4378e2522ec9c81e85ef0184fefb41840
ElyPoli <itsos.poli@gmail.com>
2023-10-20 16:10:07 +0200

    modify-header and footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,46 +1,110 @@
 <script>
-export default {
 
+export default {
     data() {
         return {
-
-        };
+            footerLinks: [
+                {
+                    title: "Home",
+                    route: "home",
+                },
+                {
+                    title: "Contatti",
+                    route: "contatti",
+                },
+                {
+                    title: "Chi Siamo",
+                    route: "chi-siamo",
+                }
+            ],
+        }
     },
-
-    methods: {
-
-
-    },
-};
+}
 </script>
 
 <template>
-    <!-- <footer class="bg-dark text-white">
-    <div class="container">
-        <div class="row row-cols-4">
-            <div class="col">
-                <h5>Contattaci:</h5>
-                <ul>
-                    <li>999999999</li>
-                    <li>348 2505200</li>
-                    <li>1515121321</li>
-                </ul>
-            </div>
-            <div class="col">
-                <h5>API Utilizzata:</h5>
-                <a href="#"></a>
-            </div>
-            <div class="col">
-                <h5>Chi Siamo</h5>
-                
-                
-            </div>
-            <div class="col">
-                
+    <footer class="text-white bg-dark fixed-bottom">
+        <div class="container p-3">
+            <div class="row row-cols-1 row-cols-sm-3">
+                <div class="col" v-for="link in footerLinks">
+                    <ul class="navbar-nav me-auto align-items-center">
+                        <li class="nav-item footer-link">
+                            <router-link class="nav-link p-1" :class="{ 'active': $route.name === link.route }"
+                                :to="{ name: link.route }">{{ link.title }}</router-link>
+                        </li>
+                    </ul>
+                </div>
+
+
+
+                <!-- <div class="col">
+                    <ul class="navbar-nav me-auto align-items-center">
+                        <li class="nav-item footer-link" v-for="link in store.menuLinks">
+                            <router-link class="nav-link p-1" :class="{ 'active': $route.name === link.route }"
+                                :to="{ name: link.route }">{{ link.title }}</router-link>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <p>Contattaci</p>
+                    <ul class="navbar-nav me-auto align-items-center">
+                        <li class="nav-item">
+                            drink@email.com
+                        </li>
+                        <li class="nav-item">
+                            348 2505200
+                        </li>
+                        <li class="nav-item">
+                            drink@email.com
+                        </li>
+                    </ul>
+                </div> -->
+
+
+
+
+
+                <!-- <div class="col">
+                    <h5>Contattaci:</h5>
+                    <ul>
+                        <li>999999999</li>
+                        <li>348 2505200</li>
+                        <li>1515121321</li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h5>API Utilizzata:</h5>
+                    <a href="#"></a>
+                </div>
+                <div class="col">
+                    <h5>Chi Siamo</h5>
+
+
+                </div>
+                <div class="col">
+
+                </div> -->
             </div>
         </div>
-    </div>
-</footer> -->
+    </footer>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+footer {
+    box-shadow: 5px 5px 10px 0px #212529;
+}
+
+.footer-link {
+    text-decoration: none;
+    color: white;
+    transition: transform 0.2s;
+
+    &:hover {
+        transform: scale(1.05);
+    }
+}
+
+.active {
+    font-weight: bold;
+}
+</style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,30 +1,40 @@
 <script>
-import { router } from '../router';
 
 export default {
+    data() {
+        return {
+            navLinks: [
+                {
+                    title: "Home",
+                    route: "home",
+                },
+                {
+                    title: "Contatti",
+                    route: "contatti",
+                },
+                {
+                    title: "Chi Siamo",
+                    route: "chi-siamo",
+                }
+            ],
+        }
+    },
 }
-
 </script>
 
 <template>
-    <nav class="navbar navbar-expand navbar-dark fixed-top shadow" aria-label="Second navbar example">
-        <div class="container" style="height: 60px;">
+    <nav class="navbar navbar-expand-sm navbar-dark fixed-top shadow" aria-label="Second navbar example">
+        <div class="container" style="min-height: 60px;">
             <a class="navbar-brand" href="#">Drinks</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02"
                 aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
             <div class="collapse navbar-collapse" id="navbarsExample02">
-                <ul class="navbar-nav me-auto gap-3">
-                    <li class="nav-item">
-                        <router-link :to="{ name: 'home' }" class="nav-link">Home</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link :to="{ name: 'contatti' }" class="nav-link">Contatti</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link :to="{ name: 'chi-siamo' }" class="nav-link">Chi Siamo</router-link>
+                <ul class="navbar-nav me-auto mb-2 mb-sm-0 align-items-center">
+                    <li class="nav-item" v-for="link in navLinks">
+                        <router-link class="nav-link" :class="{ 'active': $route.name === link.route }"
+                            :to="{ name: link.route }">{{ link.title }}</router-link>
                     </li>
                 </ul>
                 <form role="search">
@@ -35,19 +45,25 @@ export default {
     </nav>
 </template>
 
-<style>
+<style scoped lang="scss">
 .nav-link {
     text-decoration: none;
     color: white;
     transition: transform 0.2s;
-}
 
-.nav-link:hover {
-    transform: scale(1.05);
+    &:hover {
+        transform: scale(1.05);
+    }
 }
 
 .navbar {
     background-color: #4d5260;
+}
+
+.navbar-nav .nav-link.active,
+.navbar-nav .nav-link.show {
+    color: var(--bs-navbar-active-color);
+    font-weight: bold;
 }
 </style>
 
